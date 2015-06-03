3de63631a0ed32b2e24612cdbc1756ee96ab02ba
Thomas Komarnicki <komarnicki.thomas@gmail.com>
2015-06-02 21:54:13 -0400

    fix package

--- app/src/main/java/com/doglandia/geogame/MainActivity.java
+++ app/src/main/java/com/doglandia/geogame/MainActivity.java
@@ -1,17 +1,26 @@
-package doglandia.com.geogame;
+package com.doglandia.geogame;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
 
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewPager = (ViewPager) findViewById(R.id.main_pager);
+        mTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 
     @Override
